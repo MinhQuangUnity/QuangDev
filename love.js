5326def5450b1c6a5d763e7090cb5f33e67c32b8
unknown <tranminhquang6000@gmail.com>
2024-09-22 21:48:37 +0700

    Update

--- love.js
+++ love.js
@@ -8,7 +8,7 @@ import SphereObject from "./SphereObject.js";
 
 const app = new App(new THREE.Vector3(0, 0, 40));
 
-const text = new TextPrimitive("QuangDev");
+const text = new TextPrimitive("A thương e");
 text.onLoad = () => {
   text.geometry.computeBoundingBox();
   const max = text.geometry.boundingBox.max;
